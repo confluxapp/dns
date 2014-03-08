8d3e948e47cb285eb14af06dd8dcbf111d983db9
Fabian Stehle <fabi@fstehle.de>
2014-03-08 17:54:46 +0100

    Removed verification code for Google Webmaster Tools
    This reverts commit 8a7b92238c1e780d5e0261078c6856dd019b3230.

--- confluxapp.net.lua
+++ confluxapp.net.lua
@@ -8,5 +8,3 @@ github_pages(_a, "confluxapp")
 -- MX records for Zoho Mail
 mx(_a, "mx.zohomail.com", 10)
 mx(_a, "mx2.zohomail.com", 20)
-
-cname("6k5veiypzufe", "gv-7usb6ct2al3ah4.dv.googlehosted.com")
